577220c6e708b059af5ca7f9b967cdcabf09e4ed
Sanjay Ravi <imsaanjay@gmail.com>
2025-03-13 01:51:42 +0530

    click inside app working

--- src/test/java/com/mobileautomation/stepdefinitions/LoginSteps.java
+++ src/test/java/com/mobileautomation/stepdefinitions/LoginSteps.java
@@ -7,13 +7,24 @@ import org.testng.Assert;
 import com.mobileautomation.base.BaseTest;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import page_objects.loginPage.AndroidLoginPage;
 
 public class LoginSteps extends BaseTest {
     
+    private AndroidLoginPage loginPage;
+
     @Given("I am on the login screen")
     public void iAmOnTheLoginScreen() {
         System.out.println("On login screen");
         Assert.assertNotNull(driver, "Driver should not be null");
+        loginPage = new AndroidLoginPage(driver);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @When("I click on the App button")
+    public void I_enter_invalid_username_and_password() {
+        System.out.println("Clicking on App button");
+        loginPage.clickElement(loginPage.appButton);
     }
 }
